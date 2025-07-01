51c7d5340264a10bfee0a83c8aabc46f707c477d
Suryansh Singh <144591803+Suryansh064@users.noreply.github.com>
2025-07-01 14:28:14 +0530

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://snippychatf.onrender.com"],
     credentials: true
 }));
 
